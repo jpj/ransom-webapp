e610a83800a7005b6ab870a00aa82d5fac4cdd21
unknown <JanieBear@watson.(none)>
2012-04-13 07:19:25 -0400

    initial commit

---
+++ src/main/java/com/solairis/ransom/App.java
@@ -0,0 +1,13 @@
+package com.solairis.ransom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
